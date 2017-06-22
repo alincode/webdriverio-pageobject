a5750150adf675b928be6fc8356b80ce4848ff11
alincode <alincode@gmail.com>
2017-06-23 01:32:01 +0800

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,10 @@
 exports.config = {
 
+    host: 'hub.testingbot.com',
+    port: 80,
+    user: process.env.TESTINGBOT_KEY,
+    key: process.env.TESTINGBOT_SECRET,
+
     //
     // ==================
     // Specify Test Files
@@ -9,6 +14,7 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
+    
     specs: [
         './test/specs/**/*.js'
     ],
@@ -101,8 +107,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['testingbot'],
-    services: ['selenium-standalone'],
+    services: ['testingbot'],
+    // services: ['selenium-standalone'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
