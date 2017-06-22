1511d99b2230ca8cc5e491d099d8200f643926bd
alincode <alincode@gmail.com>
2017-06-23 00:18:48 +0800

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,21 +1,6 @@
 exports.config = {
 
     //
-    // =====================
-    // Server Configurations
-    // =====================
-    // Host address of the running Selenium server. This information is usually obsolete as
-    // WebdriverIO automatically connects to localhost. Also, if you are using one of the
-    // supported cloud services like Sauce Labs, Browserstack, or Testing Bot you don't
-    // need to define host and port information because WebdriverIO can figure that out
-    // according to your user and key information. However, if you are using a private Selenium
-    // backend you should define the host address, port, and path here.
-    //
-    host: '0.0.0.0',
-    port: 4444,
-    path: '/wd/hub',
-    
-    //
     // ==================
     // Specify Test Files
     // ==================
@@ -24,7 +9,6 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
         './test/specs/**/*.js'
     ],
